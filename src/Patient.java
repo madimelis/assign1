ebc9eee0d492736c60a76e813e5d92c47451f8b7
Madi Melis <250270@astanait.edu.kz>
2025-12-14 22:11:33 +0500

    Added constructor to Patient

--- src/Patient.java
+++ src/Patient.java
@@ -4,5 +4,11 @@ public class Patient {
     private int age;
     private String diagnosis;
 
+    public Patient(int id, String fullName, int age, String diagnosis) {
+        this.id = id;
+        this.fullName = fullName;
+        this.age = age;
+        this.diagnosis = diagnosis;
+    }
 
 }
