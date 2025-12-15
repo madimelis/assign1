009f76588d83541cab35665fa37f6992d8b9f1bc
Madi Melis <250270@astanait.edu.kz>
2025-12-15 20:12:21 +0500

    Created instances in Main

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,11 @@
 public class Main {
     public static void main(String[] args) {
+        Patient patient1 = new Patient(1, "Alam Arman", 20, "Food poisoning");
+        Patient patient2 = new Patient(2, "John Pork", 35, "Flu");
+        Doctor doctor1 = new Doctor(1, "John Watson", "Therapist", 7);
+        Doctor doctor2 = new Doctor(2, "Charles Pepper", "Surgeon", 20);
+        Hospital hospital = new Hospital("W Clinic", "Astana", 350);
+
 
     }
 }
