8c14ed7929a89102be6cbb6271916bea7af998e8
Madi Melis <250270@astanait.edu.kz>
2025-12-15 19:40:51 +0500

    added constructor, getters, setters to Hospital

--- src/Hospital.java
+++ src/Hospital.java
@@ -3,5 +3,17 @@ public class Hospital {
     private String location;
     private int capacity;
 
+    public Hospital(String name, String location, int capacity) {
+        this.name = name;
+        this.location = location;
+        this.capacity = capacity;
+    }
+
+    public String getName() {return name;}
+    public void setName(String name) {this.name = name;}
+    public String getLocation() {return location;}
+    public void setLocation(String location) {this.location = location;}
+    public int getCapacity() {return capacity;}
+    public void setCapacity(int capacity) {this.capacity = capacity;}
 
 }
