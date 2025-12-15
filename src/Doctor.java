f7c1b7882e02986ef772b6a8c95dcc57f393f968
Madi Melis <250270@astanait.edu.kz>
2025-12-15 18:47:56 +0500

    added fields and constructor to Doctor

--- src/Doctor.java
+++ src/Doctor.java
@@ -1,2 +1,13 @@
 public class Doctor {
+    private int id;
+    private String fullName;
+    private String specialization;
+    private int expYears;
+
+    public Doctor(int id, String fullName, String specialization, int expYears) {
+        this.id = id;
+        this.fullName = fullName;
+        this.specialization = specialization;
+        this.expYears = expYears;
+    }
 }
