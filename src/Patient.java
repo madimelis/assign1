934775a1683cdd8e893ad2a892c9fdefe7cf061f
Madi Melis <250270@astanait.edu.kz>
2025-12-15 18:40:34 +0500

    Added getters, setters, toString to Patient

--- src/Patient.java
+++ src/Patient.java
@@ -11,4 +11,21 @@ public class Patient {
         this.diagnosis = diagnosis;
     }
 
+    public int getId(){ return id;}
+    public void setId(int id) {this.id = id;}
+    public String getFullName() {return fullName;}
+    public void setFullName(String name) {this.fullName = fullName;}
+    public int getAge() {return age;}
+    public void setAge(int age) {this.age = age;}
+    public String getDiagnosis() {return diagnosis;}
+    public void setDiagnosis() {this.diagnosis = diagnosis;}
+
+    @Override
+    public String toString() {
+        return "Patient: " +
+                 " ID = " + id +
+                ", Name: " + fullName +
+                ", Age - " + age +
+                ", Diagnosis: " + diagnosis;
+    }
 }
