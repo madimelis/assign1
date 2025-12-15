c1b7ed9f01d5619e4260160a9cee71953214a7d5
Madi Melis <250270@astanait.edu.kz>
2025-12-15 19:25:09 +0500

    added getters, setters and toString to Doctor

--- src/Doctor.java
+++ src/Doctor.java
@@ -10,4 +10,22 @@ public class Doctor {
         this.specialization = specialization;
         this.expYears = expYears;
     }
+
+    public int getId() {return id;}
+    public void setId(int id) {this.id = id;}
+    public String getFullName() {return fullName;}
+    public void setFullName(String fullName) {this.fullName = fullName;}
+    public String getSpecialization() {return specialization;}
+    public void setSpecialization(String specialization) {this.specialization = specialization;}
+    public int getExpYears() {return expYears;}
+    public void setExpYears(int expYears) {this.expYears = expYears;}
+
+    @Override
+    public String toString() {
+        return "Doctor: " +
+                " ID: " + id +
+                ", Name: " + fullName +
+                ", Specialization: " + specialization +
+                ", Experience: " + expYears;
+    }
 }
--- src/Patient.java
+++ src/Patient.java
@@ -23,9 +23,9 @@ public class Patient {
     @Override
     public String toString() {
         return "Patient: " +
-                 " ID = " + id +
+                 " ID: " + id +
                 ", Name: " + fullName +
-                ", Age - " + age +
+                ", Age: " + age +
                 ", Diagnosis: " + diagnosis;
     }
 }
